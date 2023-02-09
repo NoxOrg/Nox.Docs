a8cccfccbd493a7c5dc125094d0d7b1358949781
Andre Sharpe <andre.sharpe@gmail.com>
2023-02-09 03:25:32 +0100

    Update index.php
    fixed for inividual yaml responses

--- public/workflows/index.php
+++ public/workflows/index.php
@@ -4,46 +4,79 @@
  * This script searches the current directory for files ending with '.nox.yaml'
  * and returns their name, size, and shaChecksum in a JSON format
  */
+ 
+$thisFolder = join('/', array_slice(explode('/', $_SERVER['SCRIPT_NAME']), 0, -1)).'/';
 
-$tenant = end( explode('/', trim( $_SERVER['REQUEST_URI'], '/' )));
+$targetFile = $_SERVER['REQUEST_URI'];
+
+if (substr($targetFile, 0, strlen($thisFolder)) !== $thisFolder) {
+    http_response_code(406);
+    die();
+}
+
+[$tenant,$file] = explode('/',substr($targetFile, strlen($thisFolder)));
 
 if (empty($tenant)) {
     http_response_code(404);
     die();
 }
 
-$directory = '/home/noxorg/public_ftp/incoming/' . $tenant . '/'; 
+$tenantDirectory = '/home/noxorg/public_ftp/incoming/' . $tenant . '/'; 
 
-if (!is_dir($directory)) {
-    http_response_code(404);
-    die();
+if(empty($file)) {
+
+    if (!is_dir($tenantDirectory)) {
+        http_response_code(404);
+        die();
+    }
+    header('Content-Type: text/x-yaml');
+    displayIndex($tenantDirectory);
 }
-
-// Initialize an empty array to store the file data
-$file_data = array();
-
-// Search the directory for files ending with '.nox.yaml'
-$files = glob($directory . '/*.nox.yaml');
-
-// Loop through the files
-foreach ($files as $file) {
+else {
     
-    // Get the file size
-    $size = filesize($file);
-
-    // Get the SHA checksum of the file
-    $shaChecksum = hash_file('sha256', $file);
-
-    // Add the file data to the array
-    $file_data[] = array(
-        'name' => basename($file),
-        'size' => $size,
-        'shaChecksum' => $shaChecksum
-    );
+    $fullFilePath = $tenantDirectory.'/'.$file;
+    
+    if (!file_exists($tenantDirectory)) {
+        http_response_code(404);
+        die();
+    }
+    ob_clean();
+    header('Content-Type: text/x-yaml');
+    echo file_get_contents($fullFilePath);
 }
 
-// Convert the file data to JSON and output it
-header('Content-Type: application/json');
-echo json_encode($file_data);
+function displayIndex($tenantDirectory) {
+    
+
+    // Initialize an empty array to store the file data
+    $file_data = array();
+
+    // Search the directory for files ending with '.nox.yaml'
+    $files = glob($tenantDirectory . '/*.nox.yaml');
+
+    // Loop through the files
+    foreach ($files as $file) {
+    
+        // Get the file size
+        $size = filesize($file);
+
+        // Get the SHA checksum of the file
+        $shaChecksum = hash_file('sha256', $file);
+
+        // Add the file data to the array
+        $file_data[] = array(
+            'name' => basename($file),
+            'size' => $size,
+            'shaChecksum' => $shaChecksum
+        );
+    }
+
+    // Convert the file data to JSON and output it
+    header('Content-Type: application/json');
+    echo json_encode($file_data);
+  
+}
+
+
 
 ?>
