2b3ca7d4b4b5fbc9142d9b336fb38c98b5c3a674
Andre Sharpe <andre.sharpe@gmail.com>
2023-02-21 22:19:11 +0100

    Updated root site

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,6 +10,5 @@ export default defineConfig({
 		// Enable React for the Algolia search component.
 		react(),
 	],
-	site: 'https://noxorg.dev',
-	base: '/site',
+	site: `https://noxorg.dev/site`,
 });
