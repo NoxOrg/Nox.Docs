a4432466fb1897a83b6f7967037abdaa566fa214
Jan Schutte <JanSchutte.Lands@gmail.com>
2023-08-08 06:11:45 +0200

    - added template list - added template return - added template info - improve

--- public/workflows/index.php
+++ public/workflows/index.php
@@ -9,33 +9,60 @@ $thisFolder = join('/', array_slice(explode('/', $_SERVER['SCRIPT_NAME']), 0, -1
 
 $targetFile = $_SERVER['REQUEST_URI'];
 
-if (substr($targetFile, 0, strlen($thisFolder)) !== $thisFolder) {
+if (!str_starts_with($targetFile, $thisFolder)) {
     http_response_code(406);
     die();
 }
 
-[$tenant,$requestedFile] = explode('/',substr($targetFile, strlen($thisFolder)));
+[$tenant,$requestedResource,$requestedFile] = explode('/',substr($targetFile, strlen($thisFolder)));
 
 if (empty($tenant)) {
     http_response_code(404);
     die();
 }
 
-$tenantFolder = '/home/noxorg/public_ftp/incoming/' . $tenant . '/'; 
+//$tenantFolder = '/home/noxorg/public_ftp/incoming/' . $tenant . '/';
 
-if(empty($requestedFile)) {
-
-    if (!is_dir($tenantFolder)) {
-        http_response_code(404);
-        die();
-    }
-    header('Content-Type: text/x-yaml');
-    displayIndex($tenantFolder);
+if ($requestedResource == 'templateInfo'){
+    $tenantFolder = './aabbccdd/templates';
+} else {
+    $tenantFolder = './aabbccdd/' . $requestedResource;
 }
-else {
-    
-    $requestedFilePath = $tenantFolder.'/'.$requestedFile;
-    
+
+if (!is_dir($tenantFolder)) {
+    http_response_code(404);
+    die();
+}
+
+header('Content-Type: text/x-yaml');
+
+switch ($requestedResource) {
+    case "scripts":
+        if (empty($requestedFile)) {
+            returnScriptIndex($tenantFolder);
+        } else {
+            returnFileResponse($tenantFolder, $requestedFile);
+        }
+        break;
+    case "templates":
+        if (empty($requestedFile)){
+            returnTemplateIndex($tenantFolder);
+        } else {
+            returnFileResponse($tenantFolder, $requestedFile);
+        }
+        break;
+    case "templateInfo":
+        if (empty($requestedFile)) {
+            http_response_code(404);
+            die();
+        }
+        returnTemplateInfo($tenantFolder, $requestedFile);
+        break;
+}
+
+function returnFileResponse($tenantFolder, $requestedFile): void {
+    $requestedFilePath = $tenantFolder.'/'.urldecode($requestedFile);
+
     if (!file_exists($requestedFilePath)) {
         http_response_code(404);
         die();
@@ -45,9 +72,8 @@ else {
     echo file_get_contents($requestedFilePath);
 }
 
-function displayIndex($tenantDirectory) {
-    
-
+function returnScriptIndex($tenantDirectory): void
+{
     // Initialize an empty array to store the file data
     $file_data = array();
 
@@ -64,11 +90,11 @@ function displayIndex($tenantDirectory) {
         $shaChecksum = hash_file('sha256', $file);
 
         // Add the file data to the array
-        $file_data[] = array(
+        $file_data[] = [
             'name' => basename($file),
             'size' => $size,
             'shaChecksum' => $shaChecksum
-        );
+        ];
     }
 
     // Convert the file data to JSON and output it
@@ -77,6 +103,60 @@ function displayIndex($tenantDirectory) {
   
 }
 
+function returnTemplateIndex($tenantDirectory): void {
+    $file_data = [];
+    $templateList = scanAllDir($tenantDirectory);
+    foreach($templateList as $template) {
+        $filePath = $tenantDirectory . '/' . $template;
+        $size = filesize($filePath);
+        $shaChecksum = hash_file('sha256', $filePath);
+        $file_data[] = [
+            'name' => $template,
+            'size' => $size,
+            'shaChecksum' => $shaChecksum
+        ];
+    }
 
+    // Convert the file data to JSON and output it
+    header('Content-Type: application/json');
+    echo json_encode($file_data);
+}
+
+function returnTemplateInfo($tenantDirectory, $requestedFile): void {
+    $filePath = urldecode($requestedFile);
+    $file = $tenantDirectory . '/' . $filePath;
+    $size = filesize($file);
+
+    // Get the SHA checksum of the file
+    $shaChecksum = hash_file('sha256', $file);
+
+    // Add the file data to the array
+    $file_data = [
+        'name' => $filePath,
+        'size' => $size,
+        'shaChecksum' => $shaChecksum
+    ];
+
+    // Convert the file data to JSON and output it
+    header('Content-Type: application/json');
+    echo json_encode($file_data);
+}
+
+function scanAllDir($dir): array
+{
+    $result = [];
+    foreach(scandir($dir) as $filename) {
+        if ($filename[0] === '.') continue;
+        $filePath = $dir . '/' . $filename;
+        if (is_dir($filePath)) {
+            foreach (scanAllDir($filePath) as $childFilename) {
+                $result[] = $filename . '/' . $childFilename;
+            }
+        } else {
+            $result[] = $filename;
+        }
+    }
+    return $result;
+}
 
 ?>
